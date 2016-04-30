b077543bc2867a12d674aa2e8d2ecca5f2c4f0ba
waman <waman10da@gmail.com>
2016-05-01 05:00:01 +0900

    Refactor Permutation trait and object

--- src/main/scala/org/waman/conformal/integral/Permutation.scala
+++ src/main/scala/org/waman/conformal/integral/Permutation.scala
@@ -1,38 +1,92 @@
 package org.waman.conformal.integral
 
-case class Permutation(to: List[Int]) {
+import spire.algebra.Group
 
-  def this(to: Int*) = this(to.toList)
+trait Permutation extends PartialFunction[Int, Int]{
 
-  val degree: Int = to.length
+  val degree: Int
+  protected lazy val indices: List[Int] = (0 until degree).toList
+
+  override def isDefinedAt(i: Int): Boolean = indices.contains(i)
+
+  override def apply(i: Int): Int
+  def unapply(i: Int): Int = indices.find(apply(_) == i).get
 
   def apply[E](objs: List[E]): List[E] = {
-    require(objs.size == to.size)
-
-    def construct(n: Int): List[E] = n match {
-      case this.degree => Nil
-      case _ => objs(to(n)) :: construct(n+1)
-    }
-
-    construct(0)
+    require(objs.length == degree)
+    indices.map(unapply(_)).map(objs(_))
   }
 
-  def andThen(p: Permutation): Permutation = {
+  def *(p: Permutation): Permutation = {
     require(degree == p.degree)
-    new Permutation(p(to))
+    val th = this
+    new Permutation {
+      override val degree: Int = th.degree
+      override def apply(i: Int): Int = th(p(i))
+      override def sgn: Int = th.sgn * p.sgn
+    }
   }
 
-  def *(p: Permutation): Permutation = p andThen this
+  def andThen(p: Permutation): Permutation = p * this
 
-  lazy val sgn: Int = {  // (021453)
-    def calculateSign(list: List[Int], n: Int): Int = list match {
+  def inverse: Permutation = {
+    val th = this
+    new Permutation {
+      override val degree: Int = th.degree
+      override def apply(i: Int): Int = th.unapply(i)
+      override def unapply(i: Int): Int = th.apply(i)
+      override def inverse: Permutation = th
+      override def sgn: Int = th.sgn
+    }
+  }
+
+  def sgn: Int
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Permutation =>
+      that.canEqual(this) &&
+      degree == that.degree &&
+      indices.forall(i => apply(i) == that.apply(i))
+  }
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[Permutation]
+
+  override def hashCode: Int = (degree::indices.map(apply(_))).hashCode
+
+  override def toString: String = indices.map(apply(_)).mkString("(", " ", ")")
+}
+
+object Permutation{
+
+  def identity(deg: Int): Permutation = new Permutation {
+    override val degree: Int = deg
+    override def apply(i: Int): Int = i
+    override def unapply(i: Int): Int = i
+    override def *(p: Permutation): Permutation = p
+    override def inverse: Permutation = this
+    override def sgn: Int = 1
+    override def toString: String = indices.mkString("(", " ", ")")
+  }
+
+  def apply(to: Int*): Permutation = new ListPermutation(to.toList)
+}
+
+class ListPermutation(private val to: List[Int]) extends Permutation{
+
+  override val degree: Int = to.length
+
+  override def apply(i: Int): Int = to(i)
+  override def unapply(i: Int): Int = to.indexOf(i)
+
+  override lazy val sgn: Int = {  // (021453)
+  def calculateSign(list: List[Int], n: Int): Int = list match {
       case _ :: Nil => 1
       case _ if list.head == n => calculateSign(list.tail, n+1)
       case _ =>  // args: list = List(2, 1, 4, 5, 3), n = 1
         val i = list.indexOf(n)  // i = 1
 
         val newList = exchangeHeadAndGetTail(list, i)
-          // newList = List(1, 2, 4, 5, 3) ( = List(1) ::: 2 :: List(4, 5, 3))
+        // newList = List(1, 2, 4, 5, 3) ( = List(1) ::: 2 :: List(4, 5, 3))
         -calculateSign(newList, n+1)
     }
 
@@ -43,4 +97,15 @@ case class Permutation(to: List[Int]) {
   }
 
   override def toString: String = to.mkString("(", " ", ")")
+}
+
+
+class SymmetricGroup(val degree: Int) extends Group[Permutation]{
+  override def op(x: Permutation, y: Permutation): Permutation = x * y
+  override val id: Permutation = Permutation.identity(degree)
+  override def inverse(p: Permutation): Permutation = p.inverse
+}
+
+object SymmetricGroup{
+  def apply(degree: Int): SymmetricGroup = new SymmetricGroup(degree)
 }
--- src/test/scala/org/waman/conformal/integral/PermutationSpec.scala
+++ src/test/scala/org/waman/conformal/integral/PermutationSpec.scala
@@ -4,39 +4,21 @@ import org.waman.conformal.ConformalCustomSpec
 
 class PermutationSpec extends ConformalCustomSpec{
 
-  "toString method should" - {
-
-    "create a String representation of permutation like (1 3 2)" in {
-      __Exercise__
-      val conversions =
-        Table(
-          ("p", "expected"),
-          (new Permutation(0, 2, 1)         , "(0 2 1)"),
-          (new Permutation(0, 2, 1, 4, 5, 3), "(0 2 1 4 5 3)")
-        )
-
-      __Verify__
-      forAll(conversions){ (p: Permutation, expected: String) =>
-        val sut = p.toString
-        sut should equal (expected)
-      }
-    }
-  }
-
   "apply() method should" - {
 
     "execute permutation of List" in {
-      __Exercise__
       val conversions =
         Table(
           ("p", "arg", "expected"),
-          (new Permutation(0, 2, 1)         , List("a", "b", "c")               , "acb"),
-          (new Permutation(0, 2, 1, 4, 5, 3), List("a", "b", "c", "d", "e", "f"), "acbefd")
+          (Permutation.identity(3)      , List("a", "b", "c")               , "abc"),
+          (Permutation(0, 2, 1)         , List("a", "b", "c")               , "acb"),
+          (Permutation(0, 2, 1, 4, 5, 3), List("a", "b", "c", "d", "e", "f"), "acbfde")
         )
 
-      __Verify__
       forAll(conversions){ (p: Permutation, objs: List[Any], expected: String) =>
+        __Exercise__
         val sut = p(objs)
+        __Verify__
         sut.mkString("") should equal (expected)
       }
     }
@@ -45,18 +27,43 @@ class PermutationSpec extends ConformalCustomSpec{
   "*() method should" - {
 
     "compose two permutations" in {
-      __Exercise__
       val conversions =
         Table(
           ("p0", "p1", "arg"),
-          (new Permutation(0, 2, 1), new Permutation(1, 2, 0), List("a", "b", "c")),
-          (new Permutation(0, 3, 2, 1), new Permutation(3, 1, 2, 0), List("a", "b", "c", "d"))
+          (Permutation.identity(3), Permutation(1, 2, 0)   , List("a", "b", "c")),
+          (Permutation(0, 2, 1)   , Permutation(1, 2, 0)   , List("a", "b", "c")),
+          (Permutation(0, 3, 2, 1), Permutation(3, 1, 2, 0), List("a", "b", "c", "d"))
         )
 
-      __Verify__
       forAll(conversions) { (p0: Permutation, p1: Permutation, arg: List[String]) =>
-        val sut = (p0 * p1)(arg)
+        __SetUp__
         val expected = p0(p1(arg))
+        __Exercise__
+        val sut = (p0 * p1)(arg)
+        __Verify__
+        sut should equal(expected)
+      }
+    }
+  }
+
+  "inverse() method should" - {
+
+    "return the inverse permutation" in {
+      val conversions =
+        Table(
+          ("p", "expected"),
+          (Permutation.identity(3), List("a", "b", "c")),
+          (Permutation(0, 2, 1), List("a", "b", "c")),
+          (Permutation(0, 3, 2, 1), List("a", "b", "c", "d"))
+        )
+
+      forAll(conversions) { (p: Permutation, expected: List[String]) =>
+        __SetUp__
+        val recip = p.inverse
+        val arg = p(expected)
+        __Exercise__
+        val sut = recip(arg)
+        __Verify__
         sut should equal(expected)
       }
     }
@@ -65,19 +72,66 @@ class PermutationSpec extends ConformalCustomSpec{
   "sgn() method should" - {
 
     "return sign of permutation" in {
-      __Exercise__
       val conversions =
         Table(
           ("p", "expected"),
-          (new Permutation(0, 2, 1)         , -1),
-          (new Permutation(0, 2, 1, 4, 5, 3), -1),
-          (new Permutation(2, 0, 1)         ,  1)
+          (Permutation.identity(3)      ,  1),
+          (Permutation(0, 2, 1)         , -1),
+          (Permutation(0, 2, 1, 4, 5, 3), -1),
+          (Permutation(2, 0, 1)         ,  1)
         )
 
-      __Verify__
       forAll(conversions) { (p: Permutation, expected: Int) =>
+        __Exercise__
         val sut = p.sgn
+        __Verify__
         sut should equal(expected)
+      }
+    }
+  }
+
+  "Any methods" - {
+
+    "toString method should" - {
+
+      "create a String representation of permutation like (1 3 2)" in {
+        val conversions =
+          Table(
+            ("p", "expected"),
+            (Permutation(0, 2, 1)         , "(0 2 1)"),
+            (Permutation(0, 2, 1, 4, 5, 3), "(0 2 1 4 5 3)")
+          )
+
+        forAll(conversions){ (p: Permutation, expected: String) =>
+          __Exercise__
+          val sut = p.toString
+          __Verify__
+          sut should equal (expected)
+        }
+      }
+    }
+
+    "equals() method should" - {
+
+      "return true if " in {
+        __SetUp__
+        val p0 = Permutation(0, 3, 1, 2)
+        val p1 = Permutation(0, 3, 1, 2)
+        assume(p0 ne p1)
+        __Verify__
+        p0 should equal (p1)
+      }
+    }
+
+    "hashCode property should" - {
+
+      "return the same value if two permutations are equivalent" in {
+        __SetUp__
+        val p0 = Permutation(0, 3, 1, 2)
+        val p1 = Permutation(0, 3, 1, 2)
+        assume(p0 ne p1)
+        __Verify__
+        p0.hashCode should equal (p1.hashCode)
       }
     }
   }
