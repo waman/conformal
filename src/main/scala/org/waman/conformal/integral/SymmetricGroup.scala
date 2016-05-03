426cbab4441b8cc1977a6b1fdafc33a0f47c10c9
waman <waman10da@gmail.com>
2016-05-04 00:58:35 +0900

    Refactor codes generating permutations in SymmetricGroup class

--- src/main/scala/org/waman/conformal/integral/SymmetricGroup.scala
+++ src/main/scala/org/waman/conformal/integral/SymmetricGroup.scala
@@ -26,28 +26,33 @@ class SymmetricGroup(val degree: Int) extends Group[Permutation]{
                   signed(List(2, 0, 1),  1), signed(List(2, 1, 0), -1))
     case _ =>
       @tailrec
-      def generateIntLists(seq: Seq[(List[Int], Int)], n: Int): Seq[(List[Int], Int)] = n match {
-        case 1 => seq
-        case _ =>
-          val e = degree-n+1
-          val newSeq = seq.flatMap{ case (list, sign) =>
-            (e to 0 by -1).map{ i =>
-              val newSign = if((e-i) % 2 == 0) sign else -sign
-              val (first, second) = list.splitAt(i)
-              (first ::: e :: second, newSign)
-            }
-          }
-          generateIntLists(newSeq, n-1)
-      }
+      def generateSignedPermutations(seq: Seq[SignedPermutation], n: Int): Seq[SignedPermutation] =
+        n match {
+          case 1 => seq
+          case _ =>
+            val e = degree-n+1
+            val newSeq = seq.flatMap(_.generateHigherPermutations)
+            generateSignedPermutations(newSeq, n-1)
+        }
 
-      generateIntLists(Stream((List(0), 1)), degree).map(e => signed(e._1, e._2))
+      val p1 = new SignedPermutation(List(0), 1)
+      generateSignedPermutations(Stream(p1), degree)
+  }
+
+  private[SymmetricGroup] class SignedPermutation(protected val to: List[Int], val sgn: Int)
+      extends AbstractListPermutation{
+
+    def generateHigherPermutations: Seq[SignedPermutation] =
+      (degree to 0 by -1).map{ i =>
+        val (first, second) = to.splitAt(i)
+        val newTo = first ::: degree :: second
+        val newSign = if((degree-i) % 2 == 0) sgn else -sgn
+        new SignedPermutation(newTo, newSign)
+      }
   }
 
   private[SymmetricGroup] def signed(to: List[Int], sign: Int): Permutation
-    = new SignSpecifiedPermutation(to, sign)
-
-  private[SymmetricGroup] class SignSpecifiedPermutation(protected val to: List[Int], val sgn: Int)
-    extends AbstractListPermutation
+    = new SignedPermutation(to, sign)
 }
 
 object SymmetricGroup{
